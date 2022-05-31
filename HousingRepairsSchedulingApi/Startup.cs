85e9217d679a353c0bc84e20c6458c90c9944aea
Bhavesh Patel <bhavesh.patel@madetech.com>
2022-05-31 18:11:31 +0100

    Remove health checks

--- HousingRepairsSchedulingApi/Startup.cs
+++ HousingRepairsSchedulingApi/Startup.cs
@@ -71,10 +71,10 @@ namespace HousingRepairsSchedulingApi
                 c.AddJwtSecurityScheme();
             });
 
-            var address = Configuration.GetSection(nameof(DrsOptions))[DrsOptionsApiAddressConfigurationKey];
-            var addressHost = new Uri(address).Host;
-            services.AddHealthChecks()
-                .AddTcpHealthCheck(options => options.AddHost(addressHost, 80), name: "DRS Host TCP Ping");
+            // var address = Configuration.GetSection(nameof(DrsOptions))[DrsOptionsApiAddressConfigurationKey];
+            // var addressHost = new Uri(address).Host;
+            services.AddHealthChecks();
+            //     .AddTcpHealthCheck(options => options.AddHost(addressHost, 80), name: "DRS Host TCP Ping");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
