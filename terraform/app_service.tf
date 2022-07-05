74b9e844773e0253a35c1d42935134af4135707d
Bhavesh Patel <bhavesh.patel@madetech.com>
2022-07-05 12:02:22 +0100

    Adding Terraform

---
+++ terraform/app_service.tf
@@ -0,0 +1,31 @@
+data "azurerm_service_plan" "hro-app-service-plan" {
+  name                = var.service_plan_name
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_windows_web_app_slot" "hro-scheduling-api-staging-slot" {
+  name           = "staging"
+  app_service_id = azurerm_windows_web_app.hro-scheduling-api.id
+
+  app_settings = {
+    ASPNETCORE_ENVIRONMENT    = "Staging"
+    AUTHENTICATION_IDENTIFIER = var.authentication_identifier
+    JWT_SECRET                = var.jwt_secret_staging
+    SENTRY_DSN                = var.sentry_dsn
+  }
+  site_config {}
+}
+
+resource "azurerm_windows_web_app" "hro-scheduling-api" {
+  name                = var.app_service_name
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
+  service_plan_id     = data.azurerm_service_plan.hro-app-service-plan.id
+  site_config {}
+  app_settings = {
+    ASPNETCORE_ENVIRONMENT    = "Production"
+    AUTHENTICATION_IDENTIFIER = var.authentication_identifier
+    JWT_SECRET                = var.jwt_secret_production
+    SENTRY_DSN                = var.sentry_dsn
+  }
+}
---
+++ terraform/inputs.tf
@@ -0,0 +1,24 @@
+variable "service_plan_name" {
+  type = string
+}
+variable "resource_group_name" {
+  type = string
+}
+variable "resource_group_location" {
+  type = string
+}
+variable "app_service_name" {
+  type = string
+}
+variable "authentication_identifier" {
+  type = string
+}
+variable "jwt_secret_production" {
+  type = string
+}
+variable "jwt_secret_staging" {
+  type = string
+}
+variable "sentry_dsn" {
+  type = string
+}
