226d9bfd1366eb644aac91a10355b15c81410e21
Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2022-08-04 15:14:16 +0100

    Fix formatting

--- terraform/key_vault.tf
+++ terraform/key_vault.tf
@@ -1,11 +1,11 @@
 resource "azurerm_key_vault" "hro-scheduling-api-key-vault" {
-  name                        = "hro-sched-api-key-vault"
-  location                    = var.resource_group_location
-  resource_group_name         = var.resource_group_name
-  tenant_id                   = var.azure_ad_tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-  sku_name                    = "standard"
+  name                       = "hro-sched-api-key-vault"
+  location                   = var.resource_group_location
+  resource_group_name        = var.resource_group_name
+  tenant_id                  = var.azure_ad_tenant_id
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
+  sku_name                   = "standard"
 }
 
 resource "azurerm_key_vault_access_policy" "hro-scheduling-api-key-vault-access-policy" {
