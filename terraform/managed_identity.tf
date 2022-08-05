67d8eb7ee78cf629afe53721ffa034cec92b417b
Renny Fadoju <32823756+Renny4Real@users.noreply.github.com>
2022-08-05 14:24:50 +0100

    Recreate key vault and user assigned identity

--- terraform/managed_identity.tf
+++ terraform/managed_identity.tf
@@ -1,10 +1,9 @@
-# resource "azurerm_user_assigned_identity" "hro-scheduling-api-vault-access-identity" {
-#   resource_group_name = var.resource_group_name
-#   location            = var.resource_group_location
+resource "azurerm_user_assigned_identity" "hro-scheduling-api-vault-access-identity" {
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
 
-#   name = "hro-scheduling-api-vault-access-identity"
-# }
+  name       = "hro-scheduling-api-vault-access-identity"
+  depends_on = [azurerm_resource_provider_registration.managed-identity-registration]
+}
 
-# resource "azurerm_resource_provider_registration" "managed-identity-registration" {
-#   name = "Microsoft.ManagedIdentity"
-# }
+
